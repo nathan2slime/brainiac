4ca20e1f89367e822a3fb1d00894433fa33f5f3b
Jhonathan M. <nathan3boss@gmail.com>
2025-07-20 00:07:52 -0300

    feat(ui): update styles import to use .css extension and add Card component with stories (#13)

--- apps/storybook/stories/input.stories.ts
+++ apps/storybook/stories/input.stories.ts
@@ -22,9 +22,9 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    variant: 'success',
+    variant: 'default',
     value: 'Discord',
     onChange: fn()
   }
