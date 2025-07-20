9d22a988cb75c325cf3571f6f3f45d944180051c
Jhonathan <nathan3boss@gmail.com>
2025-07-20 00:28:11 -0300

    feat(ui): add Typography component and corresponding stories; update Card styles and add AuthLayout

---
+++ apps/storybook/stories/typography.stories.tsx
@@ -0,0 +1,79 @@
+import { Typography, TypographyBaseProps } from '@iac/ui/typography'
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+const meta: Meta<typeof Typography.Body> = {
+  title: 'Components/Typography',
+  tags: ['autodocs']
+}
+
+export default meta
+
+export const H1: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Heading 1 - 40px extrabold'
+  },
+  render: (args: TypographyBaseProps) => <Typography.H1 {...args} />
+}
+
+export const H2: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Heading 2 - 32px bold'
+  },
+  render: (args: TypographyBaseProps) => <Typography.H2 {...args} />
+}
+
+export const H3: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Heading 3 - 24px semibold'
+  },
+  render: (args: TypographyBaseProps) => <Typography.H3 {...args} />
+}
+
+export const H4: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Heading 4 - 18px semibold'
+  },
+  render: (args: TypographyBaseProps) => <Typography.H4 {...args} />
+}
+
+export const H5: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Heading 5 - 16px semibold'
+  },
+  render: (args: TypographyBaseProps) => <Typography.H5 {...args} />
+}
+
+export const Body: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Body Text - 14px normal'
+  },
+  render: (args: TypographyBaseProps) => <Typography.Body {...args} />
+}
+
+export const Caption: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Caption Text - 14px normal'
+  },
+  render: (args: TypographyBaseProps) => <Typography.Caption {...args} />
+}
+
+export const Button: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Button Text'
+  },
+  render: (args: TypographyBaseProps) => <Typography.Button {...args} />
+}
+
+export const Link: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Link Text'
+  },
+  render: (args: TypographyBaseProps) => <Typography.Link {...args} />
+}
+
+export const Placeholder: StoryObj<TypographyBaseProps> = {
+  args: {
+    children: 'Placeholder Text'
+  },
+  render: (args: TypographyBaseProps) => <Typography.Placeholder {...args} />
+}
---
+++ packages/ui/src/components/typography.tsx
@@ -0,0 +1,80 @@
+import clsx from 'clsx'
+import { JSX, ReactNode } from 'react'
+import { VariantProps, tv } from 'tailwind-variants'
+
+const styles = tv({
+  variants: {
+    variant: {
+      h1: 'text-3xl font-extrabold',
+      h2: 'text-2xl font-bold',
+      h3: 'text-xl font-semibold',
+      h4: 'text-lg font-semibold',
+      h5: 'text-base font-semibold',
+      body: 'text-sm font-normal',
+      caption: 'text-sm fon-normal',
+      button: 'text-sm font-semibold uppercase',
+      link: 'text-sm font-normal underline cursor-pointer',
+      placeholder: 'text-sm font-normal italic'
+    }
+  }
+})
+
+type TypographyVariant = VariantProps<typeof styles>['variant']
+
+export type TypographyBaseProps = {
+  children: ReactNode
+  className: string
+}
+
+const createTypographyComponent = (tag: keyof JSX.IntrinsicElements, variant: TypographyVariant) => {
+  const Base = ({ children, className }: TypographyBaseProps) => {
+    const Component = tag
+
+    return <Component className={clsx(styles({ variant }), className)}>{children}</Component>
+  }
+
+  return Base
+}
+
+export const Typography = {
+  /**
+   * 40px, extrabold
+   */
+  H1: createTypographyComponent('h1', 'h1'),
+  /**
+   * 32px, bold
+   */
+  H2: createTypographyComponent('h2', 'h2'),
+  /**
+   * 24px, semibold
+   */
+  H3: createTypographyComponent('h3', 'h3'),
+  /**
+   * 18px, semibold
+   */
+  H4: createTypographyComponent('h4', 'h4'),
+  /**
+   * 16px, semibold
+   */
+  H5: createTypographyComponent('h5', 'h5'),
+  /**
+   * 14px, normal
+   */
+  Body: createTypographyComponent('p', 'body'),
+  /**
+   * 14px, normal
+   */
+  Caption: createTypographyComponent('span', 'caption'),
+  /**
+   * 14px, semibold, uppercase
+   */
+  Button: createTypographyComponent('span', 'button'),
+  /**
+   * 14px, normal, underline
+   */
+  Link: createTypographyComponent('a', 'link'),
+  /**
+   * 12px, normal, italic
+   */
+  Placeholder: createTypographyComponent('span', 'placeholder')
+}
